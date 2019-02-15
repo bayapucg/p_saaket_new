1ef94eb15e2cf6650905156aa04979b435219709
bayapucg <bayapu.edu004@gmail.com>
2019-02-15 14:31:42 +0530

    commit
    cahnges

--- application/views/home/thankyou.php
+++ application/views/home/thankyou.php
@@ -19,7 +19,7 @@
     Having trouble? <a href="">Contact us</a>
   </p>
   <p class="lead">
-    <a class="btn btn-primary btn-sm" href="<?php echo base_url(); ?>" role="button">Continue to homepage</a>
+    <a class="btn btn-primary btn-sm" href="<?php echo base_url(); ?>workshop/registration" role="button">Continue to homepage</a>
   </p>
 </div>
 </div>
